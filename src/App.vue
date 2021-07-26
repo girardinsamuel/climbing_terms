2115c8462c0c9985c04cbd3207dd879504e8249e
Samuel Girardin <samuelgirardin@pm.me>
2021-07-26 17:57:55 +0200

    add dictionary

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="flex flex-col min-h-screen pt-10 p-4 bg-yellow-400 items-center">
+    <!-- Lang selector -->
+    <nav class="flex space-x-4 mb-4" aria-label="Langs">
+      <!-- Current: "bg-indigo-100 text-indigo-700", Default: "text-gray-500 hover:text-gray-700" -->
+      <a @click="selectLang('en')"
+        class="px-3 py-2 font-medium text-sm rounded-md cursor-pointer"
+        :class="lang == 'en' ? 'bg-yellow-100 text-yellow-700' : 'text-black hover:bg-yellow-100 hover:text-yellow-700'"
+      >
+        English
+      </a>
+      <a
+        @click="selectLang('it')"
+        class="px-3 py-2 font-medium text-sm rounded-md cursor-pointer"
+        :class="lang == 'it' ? 'bg-yellow-100 text-yellow-700' : 'text-black hover:bg-yellow-100 hover:text-yellow-700'"
+      >
+        Italian
+      </a>
+      <a
+        @click="selectLang('fr')"
+        class="px-3 py-2 font-medium text-sm rounded-md cursor-pointer"
+        :class="lang == 'fr' ? 'bg-yellow-100 text-yellow-700' : 'text-black hover:bg-yellow-100 hover:text-yellow-700'"
+      >
+        French
+      </a>
+    </nav>
+    <!-- Search input -->
+    <div class="w-full sm:w-1/3 mx-auto">
+      <div class="mt-1 relative rounded-md shadow-sm">
+        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <input @input="search" type="text" name="search" id="search"
+        class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md" placeholder="Search climbing term ..." autocomplete="off">
+      </div>
+    </div>
+    <ul class="mt-10 px-0 sm:px-10 w-full">
+      <li v-for="(result, index) in results" :key="result.refIndex" class="mb-2">
+        <result :result="displayResult(result)" :first="index == 0" :lang="lang"/>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import dictionary from "./data/dictionary"
+import Fuse from 'fuse.js'
+import Result from './components/Result.vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
-</script>
+export default {
+  name: "App",
+  components: {
+    Result,
+  },
+  data () {
+    return {
+      fuse: null,
+      dictionary,
+      results: [],
+      lang: 'en'
+    }
+  },
+  created () {
+    const options = {
+      includeScore: false,
+      keys: ['word', 'translations.it', 'translations.fr', 'translations.de']
+    }
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    this.fuse = new Fuse(dictionary, options)
+  },
+  methods: {
+    search (event) {
+      const results = this.fuse.search(event.target.value)
+      this.results = results.slice(0, 8)
+    },
+    selectLang (lang) {
+      this.lang = lang
+    },
+    displayResult (fuseResult) {
+      const result = fuseResult.item
+      const translations = {...result.translations, en: result.word }
+      delete translations[this.lang]
+      return {
+        title: result.translations[this.lang] || result.word,
+        translations
+      }
+    }
+  },
 }
-</style>
+</script>
---
+++ src/components/Result.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-white rounded p-4" :class="first && 'border-2 border-red-500 border-dashed'">
+    <h1 class="font-medium text-lg mb-2">{{ result.title }}</h1>
+    <div class="flex items-center space-x-4">
+      <span class="text-gray-500 text-base" v-for="(trans, lang) in result.translations" :key="lang">
+        {{ lang.toUpperCase() }} : {{ trans }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  result: Object,
+  first: Boolean,
+  lang: String
+})
+</script>
