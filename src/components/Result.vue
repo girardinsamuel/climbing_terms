711fb8018869b8d557651ca9f06047dc87a11002
Samuel Girardin <samuelgirardin@pm.me>
2021-07-26 20:26:44 +0200

    add more features and improve design

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="bg-white rounded p-4" :class="first && 'border-2 border-red-500 border-dashed'">
+  <div class="bg-white rounded p-4" :class="result.exact && 'border-2 border-green-600 border-dashed'">
     <h1 class="font-medium text-lg mb-2">{{ result.title }}</h1>
     <div class="flex items-center space-x-4">
-      <span class="text-gray-500 text-base" v-for="(trans, lang) in result.translations" :key="lang">
-        {{ lang.toUpperCase() }} : {{ trans }}
-      </span>
+      <div class="text-gray-500 text-base flex items-center" v-for="(trans, lang) in result.translations" :key="lang">
+        <img :src="`/public/flags/${lang}.png`" class="h-4 w-4 mr-2">
+        <span>{{ trans }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -14,7 +15,5 @@ import { defineProps } from 'vue'
 
 defineProps({
   result: Object,
-  first: Boolean,
-  lang: String
 })
 </script>
